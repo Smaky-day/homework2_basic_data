df7e4af91aba62827d82e44c67c0145d7c1999c0
Smaky-day <05102024smoky@gmail.com>
2024-10-06 22:19:14 +0400

    Add files via upload

---
+++ homework2_basic_data.py
@@ -0,0 +1,22 @@
+# "1st program"
+print(9 ** 0.5 * 5)
+
+# "2st program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# "3st program"
+example1 = 2 * 2 +2
+example2 = 2 * (2 + 2)
+print(example1)
+print(example2)
+print(example1 == example2)
+
+# "4st program"
+sequence = '123.456'
+sequence2 = float(sequence) * 10
+print(int(sequence2) % 10)
+
+
+
+
+
